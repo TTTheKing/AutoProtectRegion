27f6e58dd152c757a0ea4e237494ad9f5b404481
Tobi Schmitz <tschmitz@hsr.ch>
2014-02-09 18:18:53 +0100

    initial commit

---
+++ src/com/github/diereicheerethons/autoprotectregion/AutoProtectRegion.java
@@ -0,0 +1,20 @@
+package com.github.diereicheerethons.autoprotectregion;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AutoProtectRegion extends JavaPlugin{
+	
+	public static AutoProtectRegion instance;
+	
+	@Override
+	public void onLoad(){
+		instance = this;
+	}
+	
+	@Override
+	public void onEnable(){
+		
+		
+	}
+	
+}
